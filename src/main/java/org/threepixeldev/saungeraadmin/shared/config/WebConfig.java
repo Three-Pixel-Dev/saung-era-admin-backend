0358a496e2579ded064f42942c0f091f53e5e9db
thanthtooaung <thanthtoo1285@gmail.com>
2025-12-14 14:57:48 +0700

    :sparkles: feat: added logging interceptor

---
+++ src/main/java/org/threepixeldev/saungeraadmin/shared/config/WebConfig.java
@@ -0,0 +1,29 @@
+package org.threepixeldev.saungeraadmin.shared.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.threepixeldev.saungeraadmin.shared.interceptor.LoggingInterceptor;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    private final LoggingInterceptor loggingInterceptor;
+
+    @Autowired
+    public WebConfig(LoggingInterceptor loggingInterceptor) {
+        this.loggingInterceptor = loggingInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loggingInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns(
+                        "/actuator/**",
+                        "/error",
+                        "/favicon.ico"
+                );
+    }
+}
